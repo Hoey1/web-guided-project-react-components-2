ac576235a038359963a7e22d0b4a99300f3ae857
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-12 19:17:37 +0200

    wip

--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -18,7 +18,7 @@ export default function FriendsList(props) {
       {
         // javascript expression
         friends.map(friend => {
-          debugger
+          return <Friend />
         })
       }
 
