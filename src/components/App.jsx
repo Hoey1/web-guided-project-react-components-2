e7511bf02f3625cf0a4338d123078dea566a7f99
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-12 18:36:10 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ import data from '../dummy-data/friends.js'
 export default function App() {
   // 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
+  const [friends, setFriends] = useState(data)
 
   // 4- Initialize a slice to keep track of the value of the search box
   // using an empty string as the initial value of the slice
