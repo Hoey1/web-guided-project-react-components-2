9d87e1f06e0ad7dd35d9ba21362cc6b2cccf2750
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-12 19:58:11 +0200

    wip

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -5,11 +5,14 @@ export default function Pet(props) {
   const [fav, setFav] = useState(false)
 
   // 1- Add a click handler to the button that favs the pet
+  const favHandler = evt => {
+    setFav(true)
+  }
 
   return (
     <div className='pet-friends container'>
       <h4>{pet.name} ({pet.type})</h4>
-      {fav ? '❤️' : <button>fav</button>}
+      {fav ? '❤️' : <button onClick={favHandler}>fav</button>}
     </div>
   )
 }
